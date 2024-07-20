8843c15e9a1603c85585ab55b49a74887b97782e
Rohii19 <142196054+Rohii19@users.noreply.github.com>
2024-07-21 02:05:29 +0800

    1

---
+++ MMMG/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    [Header("Connection Status")]
+    public Text connectionStatusText;
+
+    [Header("Login UI Panel")]
+    public InputField playerNameInput;
+    public GameObject Login_UI_Panel;
+
+    [Header("Game Options UI Panel")]
+    public GameObject GameOptions_UI_Panel;
+
+    [Header("Create Room UI Panel")]
+    public GameObject CreateRoom_UI_Panel;
+
+    [Header("Inside Room UI Panel")]
+    public GameObject InsideRoom_UI_Panel;
+
+    [Header("Room List UI Panel")]
+    public GameObject RoomList_UI_Panel;
+
+    [Header("Join Random Room UI Panel")]
+    public GameObject JoinRandomRoom_UI_Panel;
+
+    #region Unity Methods
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        ActivatePanel(Login_UI_Panel.name);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        connectionStatusText.text = "Connection status: " + PhotonNetwork.NetworkClientState;
+    }
+
+    #endregion
+
+    #region UI Callbacks
+    public void OnLoginButtonClicked()
+    {
+        string playerName = playerNameInput.text;
+        if (!string.IsNullOrEmpty(playerName))
+        {
+            PhotonNetwork.LocalPlayer.NickName = playerName;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        else
+        {
+            Debug.Log("Playername is invalid!");
+        }
+    }
+
+    #endregion
+
+    #region Photon Callbacks
+    public override void OnConnected()
+    {
+        Debug.Log("Connected to Internet");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " is connected to Photon");
+        ActivatePanel(GameOptions_UI_Panel.name);
+    }
+
+    #endregion
+
+    #region Public Methods
+    public void ActivatePanel(string panelToBeActivated)
+    {
+        Login_UI_Panel.SetActive(panelToBeActivated.Equals(Login_UI_Panel.name));
+        GameOptions_UI_Panel.SetActive(panelToBeActivated.Equals(GameOptions_UI_Panel.name));
+        CreateRoom_UI_Panel.SetActive(panelToBeActivated.Equals(CreateRoom_UI_Panel.name));
+        InsideRoom_UI_Panel.SetActive(panelToBeActivated.Equals(InsideRoom_UI_Panel.name));
+        RoomList_UI_Panel.SetActive(panelToBeActivated.Equals(RoomList_UI_Panel.name));
+        JoinRandomRoom_UI_Panel.SetActive(panelToBeActivated.Equals(JoinRandomRoom_UI_Panel.name));
+    }
+
+    #endregion
+}
