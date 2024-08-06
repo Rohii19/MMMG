401e0852776e7de8f1b7848e25117560556ee034
Rohii19 <142196054+Rohii19@users.noreply.github.com>
2024-08-07 06:40:27 +0800

    8

---
+++ MMMG/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSetup : MonoBehaviourPunCallbacks
+{
+    public GameObject[] FPS_Hands_ChildGameobjects;
+    public GameObject[] Soldier_ChildGameobjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (photonView.IsMine)
+        {
+            //Activate FPS Hands, Deactivate Soldier
+            foreach (GameObject gameObject in FPS_Hands_ChildGameobjects)
+            {
+                gameObject.SetActive(true);
+            }
+
+            foreach (GameObject gameObject in Soldier_ChildGameobjects)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            //Activate Soldier, Deactivate FPS Hands
+            foreach (GameObject gameObject in FPS_Hands_ChildGameobjects)
+            {
+                gameObject.SetActive(false);
+            }
+
+            foreach (GameObject gameObject in Soldier_ChildGameobjects)
+            {
+                gameObject.SetActive(true);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
