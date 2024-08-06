ced5f435ada302629c94da42af2b58e4536012b4
Rohii19 <142196054+Rohii19@users.noreply.github.com>
2024-08-07 06:14:23 +0800

    7

---
+++ MMMG/Assets/Scripts/MobileFPSGameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MobileFPSGameManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject playerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PhotonNetwork.IsConnectedAndReady)
+        {
+            if (playerPrefab != null)
+            {
+                int randomPoint = Random.Range(-10, 10);
+                PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(randomPoint, 0f, randomPoint), Quaternion.identity);
+            }
+            else
+            {
+                Debug.Log("Place playerPrefab!");
+            }
+        }
+    }
+}
+
