4b20596c7b08719a8079cc2552da924801f6c3a4
KanekiKen1111 <142958818+KanekiKen1111@users.noreply.github.com>
2024-08-09 02:09:24 +0800

    8
    8

--- MMMG/Assets/Scripts/PlayerSetup.cs
+++ MMMG/Assets/Scripts/PlayerSetup.cs
@@ -2,19 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class PlayerSetup : MonoBehaviourPunCallbacks
 {
     public GameObject[] FPS_Hands_ChildGameobjects;
     public GameObject[] Soldier_ChildGameobjects;
-
+ 
+    public GameObject playerUIPrefab;
+    private PlayerMovementController playerMovementController;
+ 
+    public Camera FPSCamera;
+ 
     // Start is called before the first frame update
     void Start()
     {
+        playerMovementController = GetComponent<PlayerMovementController>();
+ 
         if (photonView.IsMine)
         {
-            //Activate FPS Hands, Deactivate Soldier
-            foreach (GameObject gameObject in FPS_Hands_ChildGameobjects)
+	    //Activate FPS Hands, Deactivate Soldier
+	    foreach (GameObject gameObject in FPS_Hands_ChildGameobjects)
             {
                 gameObject.SetActive(true);
             }
@@ -23,9 +31,16 @@ public class PlayerSetup : MonoBehaviourPunCallbacks
             {
                 gameObject.SetActive(false);
             }
+ 
+	    //Instantiate PlayerUI
+            GameObject playerUIGameobject = Instantiate(playerUIPrefab);
+                playerMovementController.joystick = playerUIGameobject.transform.Find("Fixed Joystick").GetComponent<Joystick>();
+                playerMovementController.fixedTouchField = playerUIGameobject.transform.Find("RotationTouchField").GetComponent<FixedTouchField>(); 
+
+	    FPSCamera.enabled = true; 
         }
         else
-        {
+        { 
             //Activate Soldier, Deactivate FPS Hands
             foreach (GameObject gameObject in FPS_Hands_ChildGameobjects)
             {
@@ -35,13 +50,18 @@ public class PlayerSetup : MonoBehaviourPunCallbacks
             foreach (GameObject gameObject in Soldier_ChildGameobjects)
             {
                 gameObject.SetActive(true);
-            }
-        }
+            } 
+ 
+	    playerMovementController.enabled = false;
+            GetComponent<RigidbodyFirstPersonController>().enabled = false;
+ 
+	    FPSCamera.enabled = false;
+        } 
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
