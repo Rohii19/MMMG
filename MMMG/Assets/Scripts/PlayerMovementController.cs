b69f28b5bb6ef38fb90fe5dd72dee833037d2ff0
KanekiKen1111 <142958818+KanekiKen1111@users.noreply.github.com>
2024-08-09 03:03:45 +0800

    10
    10

--- MMMG/Assets/Scripts/PlayerMovementController.cs
+++ MMMG/Assets/Scripts/PlayerMovementController.cs
@@ -14,7 +14,8 @@ public class PlayerMovementController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rigidBodyController = GetComponent<RigidbodyFirstPersonController>();       
+        rigidBodyController = GetComponent<RigidbodyFirstPersonController>();     
+        animator = GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
@@ -27,6 +28,25 @@ public class PlayerMovementController : MonoBehaviour
     {
         rigidBodyController.joystickInputAxis.x = joystick.Horizontal;
         rigidBodyController.joystickInputAxis.y = joystick.Vertical;
-        rigidBodyController.mouseLook.lookInputAxis = fixedTouchField.TouchDist;       
+        rigidBodyController.mouseLook.lookInputAxis = fixedTouchField.TouchDist;    
+ 
+        Debug.Log("Horizontal:" + joystick.Horizontal);
+        Debug.Log("Vertical:" + joystick.Vertical);
+
+        animator.SetFloat("Horizontal",joystick.Horizontal);
+        animator.SetFloat("Vertical",joystick.Vertical);
+
+        if (Mathf.Abs(joystick.Horizontal)>0.9f || Mathf.Abs(joystick.Vertical)>0.9f)
+        {
+            //Running
+            rigidBodyController.movementSettings.ForwardSpeed = 16;
+            animator.SetBool("IsRunning",true);
+        }
+        else
+        {
+            //Not Running
+            rigidBodyController.movementSettings.ForwardSpeed = 8;
+            animator.SetBool("IsRunning", false);
+        }
     }
 }
